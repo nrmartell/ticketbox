e5b3143902a2e3db3ce3dac9ed4380e155ec3b47
Natalie Martell <nrmartell@gmail.com>
2015-11-09 14:37:18 -0600

    basic layout and sign box

--- ticketbox/client/templates/tickets/tickets_list.js
+++ ticketbox/client/templates/tickets/tickets_list.js
@@ -1,20 +1,30 @@
-var ticketsData = [
-  {
-    band: 'All American Rejects/Motion city soundtrack',
-    tour: 'Too Bad for Hell Tour',
-    year: '2003'
-  },
-  {
-    band: 'Incubus',
-    tour: 'Crow Left of the Murder',
-    year: '2003'
-  }, 
-  {
-    band: 'Rancid, The Ataris, Andrew wK',
-    tour: 'Warped Tour',
-    year: '2003'
-  }
-];
 Template.ticketsList.helpers({
-  tickets: ticketsData
+  tickets: function() {
+    return Tickets.find();
+  }
 });
+
+
+
+
+
+// var ticketsData = [
+//   {
+//     band: 'All American Rejects/Motion city soundtrack',
+//     tour: 'Too Bad for Hell Tour',
+//     year: '2003'
+//   },
+//   {
+//     band: 'Incubus',
+//     tour: 'Crow Left of the Murder',
+//     year: '2003'
+//   }, 
+//   {
+//     band: 'Rancid, The Ataris, Andrew wK',
+//     tour: 'Warped Tour',
+//     year: '2003'
+//   }
+// ];
+// Template.ticketsList.helpers({
+//   tickets: ticketsData
+// });
