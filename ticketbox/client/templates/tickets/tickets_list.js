780cab78d19563c2e4713a4e899ae4c04aa3ebe5
Natalie Martell <nrmartell@gmail.com>
2015-11-09 11:02:43 -0600

    initial commit

---
+++ ticketbox/client/templates/tickets/tickets_list.js
@@ -0,0 +1,20 @@
+var ticketsData = [
+  {
+    band: 'All American Rejects/Motion city soundtrack',
+    tour: 'Too Bad for Hell Tour',
+    year: '2003'
+  },
+  {
+    band: 'Incubus',
+    tour: 'Crow Left of the Murder',
+    year: '2003'
+  }, 
+  {
+    band: 'Rancid, The Ataris, Andrew wK',
+    tour: 'Warped Tour',
+    year: '2003'
+  }
+];
+Template.ticketsList.helpers({
+  tickets: ticketsData
+});
